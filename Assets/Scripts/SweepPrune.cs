5c2822ecb99a479327b78a39a213a0879f704d6c
regireed89 <regireed89@gmail.com>
2017-11-03 18:00:02 -0500

    fixed

--- Assets/Scripts/SweepPrune.cs
+++ Assets/Scripts/SweepPrune.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+[System.Serializable]
 public class Pair
 {
     public AABB objOne;
@@ -14,41 +15,57 @@ public class Pair
         objTwo = obj2;
     }
 }
-public class SweepPrune : MonoBehaviour {
+public class SweepPrune : MonoBehaviour
+{
 
     public List<AABB> AxisList;
     public List<AABB> ActiveList;
     public List<Pair> Reported;
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         AxisList = new List<AABB>();
         ActiveList = new List<AABB>();
         Reported = new List<Pair>();
-        AxisList.Add(FindObjectOfType<AABB>());
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ActiveList.Clear();
+        Reported.Clear();
+        AxisList = new List<AABB>(FindObjectsOfType<AABB>());
         AxisList.Sort((a, b) => a.min.x.CompareTo(b.min.x));
         ActiveList.Add(AxisList[0]);
-    }
-	
-	// Update is called once per frame
-	void Update () {
 
-        
+
         for (int i = 0; i < AxisList.Count - 1; i++)
         {
-            var current = ActiveList[i];
-            var newItem = AxisList[i+1];     
-            if(newItem.min.x > current.min.x)
+
+            var tempList = new List<AABB>();
+            if (ActiveList.Count == 0)
+                ActiveList.Add(AxisList[i]);
+            tempList.AddRange(ActiveList);
+            var newItem = AxisList[i + 1];
+            //var current = ActiveList[i];
+            foreach (var item in ActiveList)
             {
-                ActiveList.Remove(current);
+                if (newItem.min.x > item.max.x)
+                {
+                    tempList.Remove(item);
+                }
+                else
+                {
+                    tempList.Add(newItem);
+                    Reported.Add(new Pair(item, newItem));
+                }
             }
-            else
-            {
-                ActiveList.Add(newItem);
-                Reported.Add(new Pair(current, newItem));
-            }
+            ActiveList.Clear();
+            ActiveList.AddRange(tempList);
         }
-        Debug.Log(Reported.Count);
-	}
+    }
 
 
 
