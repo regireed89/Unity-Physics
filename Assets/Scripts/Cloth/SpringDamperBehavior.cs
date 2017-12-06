61f86f8a726006ab9d84127ef9947fa7218fd148
regireed89 <regireed89@gmail.com>
2017-12-05 22:27:56 -0600

    vertical dampers

--- Assets/Scripts/Cloth/SpringDamperBehavior.cs
+++ Assets/Scripts/Cloth/SpringDamperBehavior.cs
@@ -10,16 +10,31 @@ namespace HookesLaw
         [SerializeField]
         public List<ParticleBehavior> particles;
         public List<SpringDamper> springDampers;
+        GenerateParticleGrid map;
 
         public void Start()
         {
+            map = GetComponent<GenerateParticleGrid>();
+            Particle p1;
+            Particle p2;
             for (int i = 0; i <= particles.Count - 1; i++)
             {
-                Particle p1 = particles[i].particle;
-                Particle p2 = particles[i+1].particle;
-                ApplyDampers(p1, p2);
+                p1 = particles[i].particle;
+                p2 = particles[i + 1].particle;
+                if (!(i != 0 && (i + 1) % map.mapSize.x == 0))
+                {
+                    ApplyDampers(p1, p2);
+                }
             }
-              
+            for (int i = 0; i <= particles.Count - 1; i++)
+            {
+                p1 = particles[i].particle;
+                p2 = particles[i + map.mapSize.y].particle;
+                if (!(i != 0 && (i + 1) % map.mapSize.y == 0))
+                {
+                    ApplyDampers(p1, p2);
+                }
+            }
         }
 
         // Update is called once per frame
@@ -31,7 +46,7 @@ namespace HookesLaw
 
         void ApplyDampers(Particle part1, Particle part2)
         {
-            sD = new SpringDamper(part1, part2, 10, .3f, 5);
+            sD = new SpringDamper(part1, part2, 10, .3f, 2);
             springDampers.Add(sD);
         }
     }
