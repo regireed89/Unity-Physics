8a9bd7aa75d983116a9627cd644df6010cd213fa
regireed89 <regireed89@gmail.com>
2017-12-07 18:02:14 -0600

    ugh

--- Assets/Scripts/Cloth/SpringDamperBehavior.cs
+++ Assets/Scripts/Cloth/SpringDamperBehavior.cs
@@ -7,11 +7,14 @@ namespace HookesLaw
     public class SpringDamperBehavior : MonoBehaviour
     {
         SpringDamper sD;
+        public Triangle T;
         [Range(0, 10)]
         public float KS, KD, LO;
+        public Vector3 windForce;
         [SerializeField]
         public List<ParticleBehavior> particles;
         public List<SpringDamper> springDampers;
+        public List<Triangle> triangles;
         GenerateParticleGrid map;
 
         public void Start()
@@ -19,6 +22,7 @@ namespace HookesLaw
             map = GetComponent<GenerateParticleGrid>();
             Particle p1;
             Particle p2;
+            triangles = new List<Triangle>();
             //Vertical
             for (int i = 0; i < particles.Count - 1; i++)
             {
@@ -36,26 +40,65 @@ namespace HookesLaw
                 ApplyDampers(p1, p2);
                 //Diagonal Left
                 p2 = particles[i + (map.mapSize - 1)].particle;
-                ApplyDampers(p1, p2);
-                //Bending Spring 
-                p2 = particles[i + 2].particle;
-                if ((i + 2) % map.mapSize == 0)
-                    continue;
-                if (!(i != 0 && (i + 1) % map.mapSize == 0))
+                if (!(i % map.mapSize == 0))
                     ApplyDampers(p1, p2);
             }
+
             //Diagonal Right
             for (int i = 0; i < particles.Count - 1; i++)
             {
                 p1 = particles[i].particle;
-                p2 = particles[i + (map.mapSize + 1)].particle;
+                if (i + (map.mapSize + 1) > particles.Count - 1)
+                    continue;
+                else
+                {
+                    p2 = particles[i + (map.mapSize + 1)].particle;
+                }
                 if (!(i != 0 && (i + 1) % map.mapSize == 0))
                     ApplyDampers(p1, p2);
             }
 
+            //Bending Spring 
+            for (int i = 0; i < particles.Count - 1; i++)
+            {
+                p1 = particles[i].particle;
 
+                if (i + 2 > particles.Count - 1)
+                    continue;
+                else
+                {
+                    p2 = particles[i + 2].particle;
+                }
+                if ((i + 2) % map.mapSize == 0)
+                    continue;
+                if (!(i != 0 && (i + 1) % map.mapSize == 0))
+                    ApplyBendingSpring(p1, p2);
+            }
 
+            for (int i = 0; i < particles.Count - 1; i++)
+            {
+                p1 = particles[i].particle;
+                if (i + (map.mapSize * 2) > particles.Count - 1)
+                    continue;
+                else
+                {
+                    p2 = particles[i + (map.mapSize * 2)].particle;
+                }
+                ApplyBendingSpring(p1, p2);
+            }
 
+            for (int i = 0; i < particles.Count - map.mapSize; i++)
+            {
+                if (!(i != 0 && (i - 1) % map.mapSize == 0))
+                    continue;
+                else
+                {
+                    triangles.Add(new HookesLaw.Triangle(particles[i].particle, particles[i + 1].particle, particles[i + map.mapSize].particle));
+
+                    triangles.Add(new HookesLaw.Triangle(particles[i].particle, particles[i + map.mapSize].particle, particles[i + map.mapSize + 1].particle));
+                }
+                    
+            }
         }
 
         // Update is called once per frame
@@ -63,11 +106,19 @@ namespace HookesLaw
         {
             foreach (SpringDamper s in springDampers)
                 s.ComputeForce(KS, KD, LO);
+
+            foreach (Triangle t in triangles)
+                t.AerodynamicForce(windForce);
         }
 
         void ApplyDampers(Particle part1, Particle part2)
         {
             sD = new SpringDamper(part1, part2, 10, .3f, 2);
+            springDampers.Add(sD);
+        }
+        void ApplyBendingSpring(Particle part1, Particle part2)
+        {
+            sD = new SpringDamper(part1, part2, 10, .3f, 4);
             springDampers.Add(sD);
         }
     }
---
+++ Assets/Scripts/Cloth/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HookesLaw
+{
+    public class Triangle 
+    {
+
+        public float p;
+        public float Cd;
+        Particle R1;
+        Particle R2;
+        Particle R3;
+
+        public Triangle() { }
+        public Triangle(Particle p1, Particle p2, Particle p3)
+        {
+            R1 = p1;
+            R2 = p2;
+            R3 = p3;
+        }
+
+        public void AerodynamicForce(Vector3 Force)
+        {
+            var V1 = R1.velocity;
+            var V2 = R2.velocity;
+            var V3 = R3.velocity;
+
+            var V = (V1 + V2 + V3) / 3 - Force;
+            var Ao = Vector3.Cross((R2.position - R1.position).normalized, (R3.position - R1.position).normalized).magnitude;
+            var n = Vector3.Cross((R2.position - R1.position), (R3.position - R1.position)).normalized;
+            var a = Ao * (Vector3.Dot(V, n) / V.magnitude);
+            Force = -.5f * p * (V.magnitude * V.magnitude) * Cd * a * n;
+
+            R1.AddForce(Force / 3);
+            R1.AddForce(Force / 3);
+            R1.AddForce(Force / 3);
+
+        }
+    }
+}
+
+
