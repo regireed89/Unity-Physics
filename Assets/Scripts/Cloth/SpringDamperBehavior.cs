dcf6aa8600c15531465d04eca84d71adf929df1c
regireed89 <regireed89@gmail.com>
2017-12-08 21:12:36 -0600

    cloth

--- Assets/Scripts/Cloth/SpringDamperBehavior.cs
+++ Assets/Scripts/Cloth/SpringDamperBehavior.cs
@@ -60,6 +60,8 @@ namespace HookesLaw
             {
                 if (Gravity.isOn)
                     p.particle.IsGravity = true;
+                if (p.particle.position.y <= -10)
+                    p.particle.position = new Vector3(p.particle.position.x, -9, p.particle.position.z);
             }
 
         }
